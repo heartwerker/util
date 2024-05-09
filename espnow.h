82545fff8fb4bca2e7edb06cf55d691154a15134
heartwerker <heartwerk@posteo.de>
2024-05-09 15:29:47 +0200

    added functionality for multiple espnow receivers

--- espnow.h
+++ espnow.h
@@ -4,7 +4,7 @@
 #if ESP8266
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-#elif ESP32  // https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
+#elif ESP32 // https://randomnerdtutorials.com/esp-now-esp32-arduino-ide/
 #include <esp_now.h>
 #include <WiFi.h>
 esp_now_peer_info_t peerInfo;
@@ -21,8 +21,8 @@ message_generic msg;
 
 //================================================================
 
-
-uint8_t *_address_target = nullptr;
+#define NUM_MAX_TARGETS 5
+uint8_t *_targets[NUM_MAX_TARGETS] = {nullptr, nullptr, nullptr, nullptr, nullptr};
 
 typedef void (*esp_now_send_cb_t)(const uint8_t *mac_addr, esp_now_send_status_t status);
 //================================================================
@@ -56,7 +56,7 @@ void OnDataRecv(uint8_t *mac, uint8_t *incomingData, uint8_t len)
         _receiveBytes(incomingData, len);
 }
 #elif ESP32
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) 
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
     if (_receiveBytes != nullptr)
         _receiveBytes((uint8_t *)incomingData, len);
@@ -77,16 +77,18 @@ void ESPNOW_registerReceiver(unsigned char *address)
     }
 }
 
-void ESPNOW_Init(ESPNOW_RX_data_callback callback, uint8_t *target_mac_address)
+void ESPNOW_Init(ESPNOW_RX_data_callback callback, uint8_t *target_addresses[], int num_targets)
 {
     _receiveBytes = callback;
-    _address_target = target_mac_address;
+
+    for (int i = 0; i < num_targets; i++)
+        _targets[i] = target_addresses[i];
 
 #if 1 // todo is this necessary ?
     // Set device as a Wi-Fi Station
     // WiFi.mode(WIFI_STA);
     WiFi.mode(WIFI_AP_STA);
-    //WiFi.disconnect();
+    // WiFi.disconnect();
 #endif
 
     if (esp_now_init() != 0)
@@ -100,17 +102,19 @@ void ESPNOW_Init(ESPNOW_RX_data_callback callback, uint8_t *target_mac_address)
     esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
 #endif
 
-    if (_address_target != nullptr)
-    {
     // Once ESPNow is successfully Init, we will register for Send CB to
     // get the status of Trasnmitted packet
     esp_now_register_send_cb(OnDataSent);
+
+    for (int t = 0; t < num_targets; t++)
+        if (_targets[t] != nullptr)
+        {
 #if ESP8266
-        esp_now_add_peer(_address_target, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+            esp_now_add_peer(_targets[0], ESP_NOW_ROLE_COMBO, 1, NULL, 0);
 #elif ESP32
-        ESPNOW_registerReceiver(_address_target);
+            ESPNOW_registerReceiver(_targets[t]);
 #endif
-    }
+        }
 
     if (_receiveBytes != nullptr)
     {
@@ -121,26 +125,33 @@ void ESPNOW_Init(ESPNOW_RX_data_callback callback, uint8_t *target_mac_address)
     Serial.println("ESPNOW_Init() done");
 }
 
+void ESPNOW_Init(ESPNOW_RX_data_callback callback, uint8_t *target_mac_address)
+{
+    ESPNOW_Init(callback, &target_mac_address, 1);
+}
+
 void ESPNOW_sendBytes(uint8_t *data, uint8_t len)
 {
-    if (_address_target != nullptr)
-        esp_now_send(_address_target, data, len);
+    if (_targets[0] != nullptr)
+        esp_now_send(_targets[0], data, len);
 }
 
 void ESPNOW_send_generic(uint8_t *target, int index, float value)
 {
     message_generic msg;
     msg.index = index;
-    msg.value = constrain(value, 0, 1);
+    msg.value = constrain(value, -1, 1);
     esp_now_send(target, (uint8_t *)&msg, sizeof(msg));
+    char macStr[18];
+    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X", target[0], target[1], target[2], target[3], target[4], target[5]);
+    Serial.printf("ESPNOW_send_generic(%s, %d, %f)\n", macStr, index, value);
 }
-
 
 void ESPNOW_send_generic(int index, float value)
 {
     message_generic msg;
     msg.index = index;
     msg.value = value;
-    if (_address_target != nullptr)
-        esp_now_send(_address_target, (uint8_t *)&msg, sizeof(msg));
+    if (_targets[0] != nullptr)
+        esp_now_send(_targets[0], (uint8_t *)&msg, sizeof(msg));
 }
